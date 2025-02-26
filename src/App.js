701d0b1d3e11ec0c183563405bd3c9d47176469f
uvesh <145368324+uveshkhan111@users.noreply.github.com>
2025-02-26 17:42:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> This Is My Docker Project 
+          Edit <code>src/App.js</code> This Is My jenkins Project 
         </p>
         <a
           className="App-link"
