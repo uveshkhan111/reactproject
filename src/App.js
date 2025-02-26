7ce1368c06014d0497076bb2c74d9cbcf71ac79e
uvesh <145368324+uveshkhan111@users.noreply.github.com>
2025-02-26 17:43:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> This Is My jenkins Project 
+          Edit <code>src/App.js</code> This Is My jenkins pipeline Project 
         </p>
         <a
           className="App-link"
